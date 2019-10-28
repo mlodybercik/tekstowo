725017a651c456d6bad6a4aa1f15d35ba7fb6a04
krzesu0 <przemekbarcicki@gmail.com>
2019-10-28 16:37:33 +0100

    fixing searchEntry.getArtistObject

--- models/searchEntry.py
+++ models/searchEntry.py
@@ -1,7 +1,7 @@
 from . import artist
 from . import lyrics
 from . import utils
-
+from . import song
 
 class _SearchEntry():
     """Main class containing entries for search class
@@ -25,9 +25,20 @@ class ArtistEntry(_SearchEntry):
     def __repr__(self):
         return "ArtistEntryObject: {}".format(self.name)
 
+    def _parseArtistURL(self):
+        return self.url.split(",")[1].split(".")[0]
+
     def getArtistObject(self):
         # F*ck python's static variable inheritance.
-        return artist.Artist(_SearchEntry._utils.getWebsite("https://www.tekstowo.pl" + self.url))
+        return artist.Artist(_SearchEntry._utils.getWebsite("https://www.tekstowo.pl/wykonawca," + self._parseArtistURL() + ".html"))
+
+    def getAllSongs(self):
+        songs = []
+        page = self._utils.getWebsite("https://www.tekstowo.pl/piosenki_artysty,{},alfabetycznie,strona,0.html".format(self._parseArtistURL()))
+        list = page.find_all("div", "ranking-lista")[0].find_all("div", "box-przeboje")
+        for song_ in list:
+            songs.append(song.Song(song_.find_all("a", "title")[0].get("title"), song_.find_all("a", "title")[0].get("href")))
+        return songs
 
 
 class SongEntry(_SearchEntry):
